9899dac2e5cfffabd64601011942c57f9e3ee5fb
Anjali Rani <anjalirani289@gmail.com>
2020-10-29 11:09:35 -0700

    First Commit: create the directory structure and setup express server

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controller/home_controller');
+
+console.log("router loaded");
+
+router.get('/',homeController.home);
+
+module.exports=router;
